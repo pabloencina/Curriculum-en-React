e01e326bfe8d4e5f391f12cf207fee6440d56f7b
Pablo <encina.pd@gmail.com>
2022-07-15 17:22:42 -0300

    boton desplegable

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,18 +1,11 @@
 //import React from "react";
 import "../hojaDeEstiloCss/Navbar.css";
 import React, { useEffect, useState } from "react";
-import Hobbies from "./Hobbies";
 
 function Navbar() {
   const [checked, setChecked] = useState(
     localStorage.getItem("theme") === "dark" ? true : false
   );
-
-  const [show, setShow] = useState(<Hobbies />);
-
-  const mostrarComponenteHobbies = () => {
-    setShow(show);
-  };
 
   useEffect(() => {
     document
@@ -24,7 +17,7 @@ function Navbar() {
     if (checked === false) {
       localStorage.setItem("theme", "dark");
       setChecked(true);
-      setChecked("modo diurno")
+      setChecked("modo diurno");
       console.log("modo diurno");
     } else {
       localStorage.setItem("theme", "light");
@@ -36,6 +29,51 @@ function Navbar() {
   return (
     <nav className="navbar ">
       <form className="container-fluid justify-content-start">
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Mis proyectos
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+              <a
+                class="dropdown-item"
+                href="https://romantic-lichterman-e8dc4d.netlify.app/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i class="bi-github" color="red"></i> Gifos
+                
+              </a>
+            </li>
+            <li>
+              <a
+                class="dropdown-item"
+                href="https://elaborate-rabanadas-a89099.netlify.app/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i class="bi-github" color="red"></i> Color game
+              </a>
+            </li>
+            <li>
+              <a
+                class="dropdown-item"
+                href="https://62c5c6426dee8b05168be41e--statuesque-llama-7df0cc.netlify.app/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i class="bi-github" color="red"></i> Pókemon
+              </a>
+            </li>
+          </ul>
+        </div>
+
         <button
           type="button"
           className="btn btn-sm btn-outline-secondary"
@@ -43,14 +81,6 @@ function Navbar() {
           onClick={() => toggleThemeChange()}
         >
           Modo Diurno
-        </button>
-
-        <button
-          className="btn btn-sm btn-outline-secondary"
-          type="button"
-          onClick={() => console.log(mostrarComponenteHobbies)}
-        >
-          Hobbies
         </button>
       </form>
     </nav>
